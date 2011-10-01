ff4083e8f4ff6c11cb0787fa7f1e45595b171d51
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-10-01 16:39:17 -0700

    reworked json/object rendering, added full construction of example html page in construct method, some code cleanup

--- html5_core.php
+++ html5_core.php
@@ -5,30 +5,23 @@
 	A object oriented implementation of HTML 5, with json support.
 */
 
+$html = new html5_core();
+
 class html5_core{
 	public $g_attr = array('accesskey'=>'','class'=>'','contenteditable'=>array('true','false','inherit'),'contextmenu'=>'','dir'=>array('ltr','rtl','auto'),'draggable'=>array('true','false','auto'),'dropzone'=>array('copy','move','link'),'hidden'=>'hidden','id'=>'','lang'=>'','spellcheck'=> array('true','false'),'style'=>'','tabindex'=>'','title'=>'','inner'=>'');
+	public $open_tag;
 	
 	function __construct($json=NULL){
 		$this->page = new page();
 		if($json != NULL) $this->page = json_decode($json);
-		$this->page->head []=  $this->tag('meta','',array('charset'=>'utf-8'));
-		$this->page->head []= $this->tag('link','',array('rel'=>'stylesheet','href'=>'main.css'));
-		$this->page->head []= $this->tag('title','HTML5 Demos and Examples');
-		$this->page->body []= $this->tag('p','Here is something');		
+		$this->tag('meta','',array('charset'=>'utf-8'));
+		$this->tag('link','',array('rel'=>'stylesheet','href'=>'main.css'));
+		$this->tag('title','HTML5 Demos and Examples');
+		$this->tag('p','Here is something');
+		$this->tag('script','document.write("Hello World!")',array('type'=>'text/javascript'),0);
+		$this->buildPage();		
 	}
-	
-	function __destruct(){
-	// think this might work??
-	// just run a foreach and process the loop from the terminal output as example, the only tags you really need to concern yourself with (in the head) are maybe script tags, link
-	// and meta ... 
-		echo $this->tag('html',"\n\t".
-							$this->tag('head',
-												"\n\t\t". implode((is_array($this->page->head)? $this->page->head : array($this->page->head)),"\n\t\t") . "\n\t"
-										)."\n\t" . $this->tag('body',"\n\t\t".implode( (is_array($this->page->body)? $this->page->body : array($this->page->body)) ,"\n\t") . "\n\t") . "\n"
-							);			
-		self::__store();				
-	}
-	
+
 	function __store(){
 		foreach($this->page as $key=>$value){
 			if($value == NULL || $value =='')
@@ -40,79 +33,83 @@ class html5_core{
 		if($this->page->head && $this->page->body) echo json_encode($this->page);
 	
 	}
-
-	function __add($a,$value,$a_arr=NULL){
-		if(is_array($a_arr)){
-			if(array_key_exists($a,$a_arr) || array_key_exists($a,$this->g_attr) ){
-				if(is_array($a_arr[$a])){
-					$check  = $a_arr->a;
-					if(is_array($check) && in_array(trim(strtolower($value)),$check)) $this->$a = $value;
-				}else{
-				// still should do validation but i'll get around to it prolly...
-					$this->$a = $value;
+	function build_tag($inner,$a=NULL,$tag=NULL){
+		if($a != NULL){
+			foreach($a as $key=>$value){
+				// add other keys here if they do not require quotes
+					if($key != 'charset')
+						$attr .= "$key='$value' ";
+					// some values wont need quotes...
+					else
+						$attr .= "$key=$value ";	
 				}
-			}
-	}else
-		$this->$a = $value;
+		}	
+		// self close a specific tags
+		return "<$tag". ( $attr?" $attr":NULL). (in_array($tag,array('br','hr','link','meta'))?'/>' : ">$inner</$tag>" );
+	
 	}
-	function build_tag($inner ='',$a=NULL){
-		if($this->a != NULL && $a != NULL){
-			foreach($this->a as $key=>$value){
-				if(is_array($value) && array_search($a[$key],$value))
-						$this->__add($key,$a[$key]);
-				elseif(array_key_exists($key,$a))
-					$this->__add($key,$a[$key]);			
-			
-			}
+	
+	function buildPage(){
+		echo '<!DOCTYPE html> 
+<html>
+	<head>';
+	
+		foreach($this->page2['head'] as $tag_name=>$values){
+		if(count($values) == 1 && is_string($values[0]))
+				echo "\n\t\t" . $this->build_tag($values[0],'',$tag_name);
+			elseif(is_array($values[0]) && is_array($values[0][1]))
+				echo "\n\t\t" . $this->build_tag($values[0][0],$values[0][1],$tag_name);
 		}
-		$tag = $this->tag;
-		if($inner == '' && $this->inner != '')
-			$inner = $this->inner;
-		// delete first char and thats your tag...
-		foreach($this as $key=>$value){
-		// you can do better...
-			if(!is_array($value) && $key != 'inner' && $key != 'tag' && $key != 'attr' && $key != 'page' && $key != 'a' && $key != 'store'){
-				if($key != 'charset'){
-					$result .= "$key='$value' ";
-				// some values wont need quotes...
-				}else
-					$result .= "$key=$value ";	
-				// for cleanup for subsequent objects
-				// doesnt store its location ...
-				$tag = $this->tag;
-				$this->store[][$tag] = array("$key"=>"$value");
-				unset($this->$key);
-			}
+
+echo "\n\t</head>\n\t<body>\n\t";
+		foreach($this->page2['body'] as $tag_name => $values){
+			if(count($values) == 1 && is_string($values[0]))
+				echo "\n\t\t" . $this->build_tag($values[0],'',$tag_name);
+			elseif(is_array($values[0]) && is_array($values[0][1]))
+				echo "\n\t\t" . $this->build_tag($values[0][0],$values[0][1],$tag_name);
 		}
-		
-		if($inner != '' && $inner != NULL)
-			$this->store[][$tag] []=$inner;
-		$result = trim($result);
-		$tag_check = $this->tag;
-		unset($this->tag);
-		// this needs to go somewhere and be organized etc...
-		// a closed tag wont have an inner so we can automatically not show it..
-		return "<$tag". ($result? " $result":NULL). (in_array($tag_check,array('br','hr','link','meta'))?'/>' : ">$inner</$tag>" ); 
-		
+
+		echo "\n\t</body>\n</html>";		
 	}
-	function tag($tag,$inner=NULL,$a=NULL){
-		if(!$this->tag){
-			$this->tag = $tag;
+	
+	function tag($tag,$inner=NULL,$a=NULL,$to_head=NULL){
+	// to head is for putting tags in the header (specifically for the script tag
+		if($tag){
+		// loads the tag attribute list.. could probably try to make this static so we dont have to call a new tag_class_name
+			//$this->tag = $tag;
 			$tag_class_name = '_'.$tag;
 			$this->tag = new $tag_class_name;
 			$this->a = $this->tag->a;
-			$this->tag = $tag;
+			//$this->tag = $tag;
+			
+		//	if($this->open_tag == 'open'){
+		//		$this->page->body->$tag []= array($inner,$a);
+		//	}
+			
 			}
-		else
-			return 'Current tag not closed. Please run build_tag() on object';
-			// or begin to track inner tags ?
+		if($tag == 'meta' || $tag == 'link' || $tag == 'title' || $to_head == 1 ){
+			$loc = 'head';
+		}else{
+			$loc = 'body';
+		}
+		if($a != NULL ){
 
-			return $this->build_tag($inner,$a);
-	}		
-
+			foreach($a as $attr=>$value){
+				unset($stop);
+				if( array_key_exists($attr,$this->a) && is_array($this->a[$attr]) && array_search($attr,$this->a)){
+					$this->page2[$loc][$tag] []= array_shift(array_filter(array($inner,$a)));
+						$stop = true;
+				}
+				if( array_key_exists($attr,$this->a)  && !$stop){
+					$this->page2[$loc][$tag] []= array_filter(array($inner,$a));
+				}			
+			}
+		}else{
+			$this->page2[$loc][$tag] []= array_shift(array_filter(array($inner,$a)));
+		}				
+				
 }
-
-	
+}
 class page{public $head,$body;}
 class _a{public $a = array('href' => '','hreflang'=>'','title'=>'','media'=>'','rel'=> array('alternate','author','bookmark','external','help','license','next','nofollow','noreferrer','prefetch','prev','search','sidebar','tag'),'target'=>array('_blank','_parent','_self','_top','framename'),'type'=>'MIME_type');}
 class _abbr{}
@@ -219,7 +216,7 @@ class _ruby{}
 class _s{}
 class _samp{}
 //	Note: If the "src" attribute is present, the <script> element must be empty.
-class _script{public $a= array('async'=>'async','defer'=>'defer','type'=>'MIME_type','charset'=>'','src'=>'');	}
+class _script{public $a= array('async'=>'async','defer'=>'defer','type'=>'','charset'=>'','src'=>'');	}
 class _select{public $a = array('autofocus'=>'autofocus','disabled'=>'disabled','form'=>'','multiple'=>'','name'=>'','size'=>'');}
 class _section{}
 class _small{}
