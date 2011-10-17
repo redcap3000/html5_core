7edcb0074632fcede396c133cfad03930203f2d6
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-10-16 23:48:21 -0700

    rewrote json clean structure, and import functions, store tags as sleek json structures

--- json_core.php
+++ json_core.php
@@ -1,37 +1,101 @@
 <?php
 // Class for json functions.. Probably will put the couch class here too.
 class json_core{
-	public static function _page($json,$from_file=false){
-	echo $json;
-		if($from_file != false)
-			// attempt to load the json as a file path
-			$json = file_get_contents($json);	
-		$json = json_decode($json);
-		
-		if(is_object($json)){
-			$result = new page($json->head,$json->body,$json->title,$json->b_at,$json->h_at);
-		}else{
-			$result = "\nInvalid json, or json file path\n";
+	
+	public static function json_clean($object){
+	// basically returns an array given an object ? and does the 't' tag processing
+	// to give a cleaner json structure
+//	print_r($object);
+		if(is_object($object)){
+		// process the variables we want on top all the time ...
+		// 'tag'... and maaybbee 'inner' ..
+			$r [0] = $object->t;
+			unset($object->t);
+			foreach($object as $x=>$y){
+			// better way to do this ?, without reordering the object.. prolly not :(
+				if(is_string($y)){
+					$r[]="$x:$y";
+					unset($object->$x);
+				}
+			}
+			
+			foreach($object as $x=>$y){
+				if($x == 't'){
+					$r [0]=$y;
+					unset($y);
+				}elseif($x == 'in' && is_array($y)){
+				
+					foreach($y as $iK =>$iV){
+					// flips the order ... hmph
+						$r[]= self::json_clean($iV);
+						//$object[$x] [] =self::json_clean($iV); 
+					}
+				
+				}elseif(is_object($y)){
+					$r[0] = ltrim(get_class($object),'_');
+					$r [] = self::json_clean($y);
+				}elseif(is_string($y) && $x = 'in'){
+					$r[] = "in:$y";
+				}
+			
+			}
 		}
+		elseif(is_array($object)){
+			foreach($object as $x=>$y){
+				if($y->in){
+					if(is_array($y->in)){
+						foreach($y->in as $inKey=>$inVal){
+// this may need some testing...
+							$r [0] = $inVal->t;
+
+							$r [] =self::json_clean($inVal);
+							//unset($intVal->t);
+							
+							unset($y->t);
+							$r []=self::json_clean($y);
+							}
+							
+					}elseif(is_string($y->in)){
+					
+						$r[]= 'in:'.$y->in;
+					}
+					unset($y->in); 
+				}else{
+					unset($y->t);
+				}
+				unset($y->t);
+				$r []=self::json_clean($y);
+			}
 		
-		return $result->make_page();
+		}
+		return $r;
+		
+	
 	}
+	
 	// because json wont store the name of an object class, when reading a json object 
 	// back in use a t parameter as the objects class (or tag)
-	public static function std_to_tag($obj){
-		$new_class = '_'.$obj->t;
-		if(is_object($obj->in))
-		// convert std class into its class tag
-			$obj->in = self::std_to_tag($obj->in);
-		if(is_object($obj->at)){
-		// convert object into assoc. array
-			foreach($obj->at as $x=>$y)
-				$arr[$x]=$y;
-			$obj->at = $arr;
-			unset($arr);
-		}
-		// attributes are returned as an object instead of array :(
-		return new $new_class($obj->in,$obj->at);
+	
+	public static function json_to_page($json){
+		if(!is_array($json) && is_string($json)) $json = json_decode($json,true);
+		return self::ar_to_tag($json);
 	}
-
+	
+	private static function ar_to_tag($array){
+		$tag = '_'. $array[0];
+		foreach($array as $loc=>$val){
+			if($loc != 0){
+				if(is_array($val)){
+					$r ['in'][]= self::ar_to_tag($val);
+				}else{
+					$temp = explode(':',$val,2);
+					$r [$temp[0]]= $temp[1];
+					unset($temp);
+				}
+			}
+		}
+		if($tag != '_')
+		return new $tag(NULL,$r);
+		else return $r;
+	}
 }
